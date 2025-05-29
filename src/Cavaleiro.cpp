97a5ffdf909ec4eff63e5935765e15ab217b6623
alvino2002 <alvino_yu@hotmail.com>
2025-05-29 10:53:46 -0300

    Update and rename teste2 to src/Cavaleiro.cpp

---
+++ src/Cavaleiro.cpp
@@ -0,0 +1,185 @@
+#include "Cavaleiro.h"
+#include "GerenciadorTempo.h"
+#include "GerenciadorAnimacao.h"
+#include <iostream>
+
+namespace Entidades
+{
+	namespace Personagens
+	{
+		Cavaleiro::Cavaleiro(sf::Vector2f tamanho, sf::Vector2f posicaoInicial, sf::Vector2f velocidade, int vida,
+			sf::Texture* texture, sf::Vector2u imageCount) :
+			Jogador(tamanho, posicaoInicial, velocidade, vida),
+			estaAtacando(false),
+			dano(4),
+			pulo(60.0f),
+			energia(10),
+			tempoRecuperar(),
+			duracaoRecuperacao(5.0f),
+			tempoAtaque(),
+			recargaAtaque(0.5f),
+			ataquePronto(true)
+		{
+			pGA = new Gerenciadores::GerenciadorAnimacao();
+			corpo.setTexture(texture);
+			pGA->pegarAnimacao(texture, imageCount);
+		}
+
+		Cavaleiro::~Cavaleiro()
+		{
+		}
+
+		void Cavaleiro::executar()
+		{
+
+			velAtual.x = 0.0f;
+
+			/*Movimento do cavaleiro*/
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+			{
+				velAtual.x -= velocidade.x;
+				olhandoDireita = false;
+			}
+
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			{
+				velAtual.x += velocidade.x;
+				olhandoDireita = true;
+			}
+
+			/*Pulo do cavaleiro*/
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && (naSuperficie || naCaixa || naLava))
+			{
+				velAtual.y = -sqrtf(2.0f * gravidade * pulo);
+
+				if (naSuperficie) // Em cima da plataforma
+				{
+					naSuperficie = false;
+				}
+
+				if (naCaixa) // Em cima da caixa
+				{
+					naCaixa = false;
+				}
+
+				if (naLava) // Em cima da lava
+				{
+					naLava = false;
+				}
+			}
+
+			float deltaTime = pGT->getDeltaTime();
+
+			if (naSuperficie || naCaixa || naLava)
+			{
+				velAtual.y = 0;
+			}
+			else
+			{
+				velAtual.y += gravidade * deltaTime;
+			}
+
+			/*Ataque do jogador*/
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::R) && ataquePronto && energia)
+			{
+				setEstaAtacando(true);
+				ataquePronto = false;
+				tempoAtaque.restart();
+				operator--();
+			}
+
+			else if (tempoAtaque.getElapsedTime().asSeconds() >= recargaAtaque)
+			{
+				ataquePronto = true;        // Libera o próximo ataque
+				setEstaAtacando(false);     // Desativa o estado de ataque
+			}
+
+			if (energia == 0 && tempoRecuperar.getElapsedTime().asSeconds() >= duracaoRecuperacao)
+			{
+				recuperar();
+			}
+
+			corpo.move(velAtual * deltaTime);
+
+			/*Invulnerabilizar*/
+			if (invulneravel && tempoInvulneravel.getElapsedTime().asSeconds() >= duracaoInvulneravel)
+			{
+				setInvulneravel(false); // Desativa a invulnerabilidade
+			}
+
+			if (invulneravel)
+			{
+				// Piscar o sprite (ex.: alternar visibilidade a cada 0.1s)
+				float tempoPiscar = tempoInvulneravel.getElapsedTime().asSeconds();
+				bool visivel = static_cast<int>(tempoPiscar * 10) % 2 == 0; // Pisca rápido
+				corpo.setFillColor(visivel ? sf::Color::White : sf::Color::Transparent);
+			}
+
+			else
+			{
+				corpo.setFillColor(sf::Color::White); // Volta ao normal
+			}
+
+			if (vida == 0)
+			{
+				setVivo(false);
+			}
+
+			/*Atualizar a animacao*/
+			unsigned int linha = 0;
+
+			if (getEstaAtacando())
+			{
+				linha = 2; // Atacando
+			}
+
+			else if (velAtual.x != 0.0f) 
+			{
+				linha = 1; // Correndo
+			}
+
+			else {
+				linha = 0; // Parado
+			}
+
+			pGA->atualizar(linha, olhandoDireita);
+
+			corpo.setTextureRect(pGA->getUvRect());
+		}
+
+		bool Cavaleiro::getEstaAtacando()
+		{
+			return estaAtacando;
+		}
+
+		void Cavaleiro::setEstaAtacando(bool atacando)
+		{
+			estaAtacando = atacando;
+		}
+
+		int Cavaleiro::getDano()
+		{
+			return dano;
+		}
+
+		void Cavaleiro::operator--()
+		{
+			if (energia > 0)
+			{
+				energia--;
+
+				if (energia == 0)
+				{
+					tempoRecuperar.restart();
+				}
+			}
+		}
+
+		void Cavaleiro::recuperar()
+		{
+			energia = 10;
+		}
+	}
+}
+
+
