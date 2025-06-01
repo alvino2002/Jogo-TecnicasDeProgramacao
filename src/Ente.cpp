3da696f9379702acacd3e7e27a407520160e3c26
alvino2002 <alvino_yu@hotmail.com>
2025-06-01 14:07:06 -0300

    namespace adicionado

--- src/Ente.cpp
+++ src/Ente.cpp
@@ -3,23 +3,26 @@
 #include "GerenciadorEvento.h"
 #include "GerenciadorTempo.h"
 #include "GerenciadorGrafico.h"
+#include "GerenciadorEstado.h"
 
 
-namespace Masmorra
+using namespace Masmorra;
+
+Ente::Ente() :
+	pGG(nullptr),
+	pGE(nullptr),
+	pGC(nullptr),
+	pGT(nullptr),
+	pGEs(nullptr)
 {
-	Ente::Ente() :
-		pGG(nullptr),
-		pGE(nullptr),
-		pGC(nullptr),
-		pGT(nullptr)
-	{
-		pGG = Gerenciadores::GerenciadorGrafico::getGerenciadorGrafico();
-		pGE = Gerenciadores::GerenciadorEvento::getGerenciadorEvento();
-		pGC = Gerenciadores::GerenciadorColisao::getGerenciadorColisao();
-		pGT = Gerenciadores::GerenciadorTempo::getGerenciadorTempo();
-	}
+	pGG = Gerenciadores::GerenciadorGrafico::getGerenciadorGrafico();
+	pGE = Gerenciadores::GerenciadorEvento::getGerenciadorEvento();
+	pGC = Gerenciadores::GerenciadorColisao::getGerenciadorColisao();
+	pGT = Gerenciadores::GerenciadorTempo::getGerenciadorTempo();
+	pGEs = Gerenciadores::GerenciadorEstado::getGerenciadorEstado();
 
-	Ente::~Ente()
-	{
-	}
+}
+
+Ente::~Ente()
+{
 }
