a6180de9ee26420ae64970fa9ab7eb09c7c5e2d8
alvino2002 <alvino_yu@hotmail.com>
2025-06-01 12:55:28 -0300

    namespace adicionado

--- headers/Cavaleiro.h
+++ headers/Cavaleiro.h
@@ -1,36 +1,40 @@
 #pragma once
 #include "Jogador.h"
 #include "GerenciadorAnimacao.h"
+#include "GerenciadorTempo.h"
 
-namespace Entidades
+
+namespace Masmorra
 {
-	namespace Personagens
+	namespace Entidades
 	{
-		class Cavaleiro : public Jogador
+		namespace Personagens
 		{
-		private:
-			bool estaAtacando;
-			int dano;
-			float pulo;
-			int energia;
-			sf::Clock tempoRecuperar; // Tempo para recuperar energia
-			float duracaoRecuperacao;
-			sf::Clock tempoAtaque;  // Temporizador para o ataque
-			float recargaAtaque;  // Tempo de espera entre ataques
-			bool ataquePronto;
-			Gerenciadores::GerenciadorAnimacao* pGA;
+			class Cavaleiro : public Jogador
+			{
+			private:
+				bool estaAtacando;
+				int dano;
+				float pulo;
+				int energia;
+				sf::Clock tempoRecuperar; // Temporalizador para a energia
+				float duracaoRecuperacao; // Tempo para recuperar energia
+				sf::Clock tempoAtaque;  // Temporizador para o ataque
+				float recargaAtaque;  // Tempo de espera entre ataques
+				bool ataquePronto;
+				Gerenciadores::GerenciadorAnimacao* pGA;
 
-		public:
-			Cavaleiro(sf::Vector2f tamanho, sf::Vector2f posicaoInicial, sf::Vector2f velocidade, int vida,
-				sf::Texture* texture, sf::Vector2u imageCount);
-			~Cavaleiro();
-			void executar();
-			bool getEstaAtacando();
-			void setEstaAtacando(bool atacando);
-			int getDano();
-			void operator--();
-			void recuperar();
-		};
+			public:
+				Cavaleiro(sf::Vector2f tamanho, sf::Vector2f posicaoInicial, sf::Vector2f velocidade, int vida,
+					      sf::Texture* texture, sf::Vector2u imageCount);
+				~Cavaleiro();
+				void executar();
+				bool getEstaAtacando()const;
+				void setEstaAtacando(bool atacando);
+				int getDano()const;
+				void operator--();
+				void recuperar();
+			};
+		}
 	}
 }
-
