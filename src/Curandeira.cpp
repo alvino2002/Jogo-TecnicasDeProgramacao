59e58fe01fb0eb3b12249b99f7d793ea4697311c
alvino2002 <alvino_yu@hotmail.com>
2025-06-08 19:38:00 -0300

    Update Curandeira.cpp

--- src/Curandeira.cpp
+++ src/Curandeira.cpp
@@ -1,163 +1,144 @@
 #include "Curandeira.h"
-#include "GerenciadorTempo.h"
 
 
-namespace Entidades
+using namespace Masmorra::Entidades::Personagens;
+
+
+Curandeira::Curandeira(sf::Vector2f tamanho, sf::Vector2f posicaoInicial, sf::Vector2f velocidade, int vida,
+	sf::Texture* texture, sf::Vector2u imageCount) :
+	Jogador(tamanho, posicaoInicial, velocidade, vida),
+	pulo(80.0f),
+	olhandoDireita(true),
+	tempoCura(),
+	recargaCura(30.0f),
+	curaPronta(true),
+	curaAliado(false),
+	pCavaleiro()
 {
-	namespace Personagens
+	pGA = new Gerenciadores::GerenciadorAnimacao();
+	corpo.setTexture(texture);
+	pGA->pegarAnimacao(texture, imageCount);
+}
+
+Curandeira::~Curandeira()
+{
+	pGA = nullptr;
+	pCavaleiro = nullptr;
+}
+
+void Curandeira::setCavaleiro(Cavaleiro* pCa)
+{
+	if (pCa)
 	{
-		Curandeira::Curandeira(sf::Vector2f tamanho, sf::Vector2f posicaoInicial, sf::Vector2f velocidade, int vida,
-			sf::Texture* texture, sf::Vector2u imageCount) :
-			Jogador(tamanho, posicaoInicial, velocidade, vida),
-			pulo(80.0f),
-			olhandoDireita(true),
-			tempoCura(),
-			recargaCura(30.0f),
-			curaPronta(true),
-			curaAliado(false),
-			pCavaleiro()
+		pCavaleiro = pCa;
+	}
+}
+
+void Curandeira::executar()
+{
+	velAtual.x = 0.0f;
+
+	/*Movimento da curandeira*/
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		velAtual.x -= velocidade.x;
+		olhandoDireita = false;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		velAtual.x += velocidade.x;
+		olhandoDireita = true;
+	}
+
+	/*Atualizar animacao*/
+	unsigned int linha = 0;
+
+	corpo.setTextureRect(pGA->getFrameAtual());
+	pGA->atualizar(2, olhandoDireita);
+
+	/*Pulo da curandeira*/
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && (naSuperficie))
+	{
+		velAtual.y = -sqrtf(2.0f * gravidade * pulo);
+
+		if (naSuperficie) // Em cima da plataforma
 		{
-			pGa = new Gerenciadores::GerenciadorAnimacao();
-			corpo.setTexture(texture);
-			pGa->pegarAnimacao(texture, imageCount);
+			naSuperficie = false;
 		}
+	}
 
-		Curandeira::~Curandeira()
+	float deltaTime = pGT->getDeltaTempo();
+
+	if (naSuperficie)
+	{
+		velAtual.y = 0;
+	}
+	else
+	{
+		velAtual.y += gravidade * deltaTime;
+	}
+
+	corpo.move(velAtual * deltaTime);
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && curaPronta)
+	{
+		curaPronta = false;
+		curar();
+		tempoCura.restart();
+	}
+
+	else if (tempoCura.getElapsedTime().asSeconds() >= recargaCura)
+	{
+		curaPronta = true;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+	{
+		if (!curaAliado) 
 		{
+			curarAliado();
+			curaAliado = true;
 		}
-
-		void Curandeira::setCavaleiro(Cavaleiro* pCa)
-		{
-			if (pCa)
-			{
-				pCavaleiro = pCa;
-			}
-		}
-
-		void Curandeira::executar()
-		{
-
-			velAtual.x = 0.0f;
-
-			/*Movimento da curandeira*/
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-			{
-				velAtual.x -= velocidade.x;
-				olhandoDireita = false;
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-			{
-				velAtual.x += velocidade.x;
-				olhandoDireita = true;
-			}
-
-			/*Atualizar animacao*/
-			unsigned int linha = 0;
-
-			if (velAtual.x != 0.0f)
-			{
-				linha = 2; // Correndo
-			}
-
-			corpo.setTextureRect(pGa->getUvRect());
-			pGa->atualizar(2, olhandoDireita);
-
-			/*Pulo da curandeira*/
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && (naSuperficie || naCaixa || naLava))
-			{
-				velAtual.y = -sqrtf(2.0f * gravidade * pulo);
-
-				if (naSuperficie) // Em cima da plataforma
-				{
-					naSuperficie = false;
-				}
-
-				if (naCaixa) // Em cima da caixa
-				{
-					naCaixa = false;
-				}
-
-				if (naLava) // Em cima da lava
-				{
-					naLava = false;
-				}
-			}
-
-			float deltaTime = pGT->getDeltaTime();
-
-			if (naSuperficie || naCaixa || naLava)
-			{
-				velAtual.y = 0;
-			}
-			else
-			{
-				velAtual.y += gravidade * deltaTime;
-			}
-
-			
-			corpo.move(velAtual * deltaTime);
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && curaPronta)
-			{
-				curaPronta = false;
-				curar();
-				tempoCura.restart();
-			}
-
-			else if (tempoCura.getElapsedTime().asSeconds() >= recargaCura)
-			{
-				curaPronta = true;
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
-			{
-				if (!curaAliado)
-				{
-					curarAliado();
-					curaAliado = true;
-				}
-			}
-			else
-			{
-				curaAliado = false;
-			}
+	}
+	else
+	{
+		curaAliado = false;
+	}
 
 
-			if (invulneravel && tempoInvulneravel.getElapsedTime().asSeconds() >= duracaoInvulneravel)
-			{
-				setInvulneravel(false);
-			}
+	if (invulneravel && tempoInvulneravel.getElapsedTime().asSeconds() >= duracaoInvulneravel)
+	{
+		setInvulneravel(false);
+	}
 
-			if (invulneravel)
-			{
-				float tempoPiscar = tempoInvulneravel.getElapsedTime().asSeconds();
-				bool visivel = static_cast<int>(tempoPiscar * 10) % 2 == 0;
-				corpo.setFillColor(visivel ? sf::Color::White : sf::Color::Transparent);
-			}
-			else
-			{
-				corpo.setFillColor(sf::Color::White);
-			}
+	if (invulneravel)
+	{
+		float tempoPiscar = tempoInvulneravel.getElapsedTime().asSeconds();
+		bool visivel = static_cast<int>(tempoPiscar * 10) % 2 == 0;
+		corpo.setFillColor(visivel ? sf::Color::White : sf::Color::Transparent);
+	}
+	else
+	{
+		corpo.setFillColor(sf::Color::White);
+	}
 
-			if (vida <= 0)
-			{
-				setVivo(false);
-			}
+	if (vida <= 0)
+	{
+		setVivo(false);
+	}
 
-		}
-		void Curandeira::curar() // Curar a si mesma
-		{
-			vida += 5;
-		}
+}
+void Curandeira::curar() // Curar a si mesma
+{
+	vida += 5;
+}
 
-		void Curandeira::curarAliado()
-		{
-			if (pCavaleiro)
-			{
-				vida -= 10; // Fornece sua propria energia vital
-				pCavaleiro->setVida(pCavaleiro->getVida() + 10);
-			}
-		}
+void Curandeira::curarAliado()
+{
+	if (pCavaleiro)
+	{
+		vida -= 10; // Fornece sua propria energia vital
+		pCavaleiro->setVida(pCavaleiro->getVida() + 10);
 	}
 }
