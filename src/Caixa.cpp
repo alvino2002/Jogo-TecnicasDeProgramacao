1289c015eaa3602436135c007a2bc28613c9bc56
alvino2002 <alvino_yu@hotmail.com>
2025-06-08 19:37:19 -0300

    Update Caixa.cpp

--- src/Caixa.cpp
+++ src/Caixa.cpp
@@ -1,5 +1,5 @@
 #include "Caixa.h"
-
+#include <iostream>
 
 using namespace Masmorra::Entidades::Obstaculos;
 
@@ -21,71 +21,72 @@ void Caixa::executar()
 {
 	if (!naSuperficie)
 	{
-		float deltaTime = pGT->getDeltaTime();
+		float deltaTime = pGT->getDeltaTempo();
 		aplicarGravidade(deltaTime);
 	}
+	naSuperficie = false;
 }
 
-void Caixa::interagir(Personagens::Jogador* pJ)
+void Caixa::interagir(Entidade* pE)
 {
-	sf::Vector2f posicao_Jogador = pJ->getCorpo().getPosition();
-	sf::Vector2f tamanho_Jogador = pJ->getCorpo().getSize();
+	sf::Vector2f posicao_Entidade = pE->getCorpo().getPosition();
+	sf::Vector2f tamanho_Entidade = pE->getCorpo().getSize();
 	sf::Vector2f posicao_Caixa = getCorpo().getPosition();
 	sf::Vector2f tamanho_Caixa = getCorpo().getSize();
 
-	bool ehCavaleiro = dynamic_cast<Personagens::Cavaleiro*>(pJ) != nullptr;
+	bool ehCavaleiro = dynamic_cast<Personagens::Cavaleiro*>(pE) != nullptr;
 
 	if (pGC->getIntersectX() > pGC->getIntersectY())
 	{
 		if (ehCavaleiro) // Apenas o cavaleiro consegue empurrar a caixa
 		{
-			float deltaTime = pGT->getDeltaTime();
+			float deltaTime = pGT->getDeltaTempo();
 			float velocidadeX = pCavaleiro->getVelocidadeX();
 			float deslocamentoCaixa = velocidadeX * 0.5f * deltaTime; // Ha uma desaceleracao ao empurrar
 
-			if (posicao_Jogador.x < posicao_Caixa.x) // Empurra para direita
+			if (posicao_Entidade.x < posicao_Caixa.x) // Empurra para direita
 			{
-				posicao_Jogador.x = posicao_Caixa.x - tamanho_Jogador.x;
+				posicao_Entidade.x = posicao_Caixa.x - tamanho_Entidade.x;
 				posicao_Caixa.x += deslocamentoCaixa;
 			}
 			else  // Empurra para esquerda
 			{
-				posicao_Jogador.x = posicao_Caixa.x + tamanho_Caixa.x;
+				posicao_Entidade.x = posicao_Caixa.x + tamanho_Caixa.x;
 				posicao_Caixa.x -= deslocamentoCaixa;
 			}
 
 			getCorpo().setPosition(posicao_Caixa);
 		}
 
-		else // Curandeira
+		else 
 		{
-			if (posicao_Jogador.x < posicao_Caixa.x)
+			if (posicao_Entidade.x < posicao_Caixa.x)
 			{
-				posicao_Jogador.x += pGC->getIntersectX();
+				posicao_Entidade.x += pGC->getIntersectX();
 			}
 			else
 			{
-				posicao_Jogador.x -= pGC->getIntersectX();
+				posicao_Entidade.x -= pGC->getIntersectX();
 			}
 		}
 	}
 	else // Colisão vertical
 	{
-		if (posicao_Jogador.y < posicao_Caixa.y)
+		if (posicao_Entidade.y < posicao_Caixa.y)
 		{
-			posicao_Jogador.y = posicao_Caixa.y - tamanho_Jogador.y;
+			posicao_Entidade.y = posicao_Caixa.y - tamanho_Entidade.y;
 
-			pJ->setNaCaixa(true);
+			pE->setNaSuperficie(true);
 		}
 		else
 		{
-			posicao_Jogador.y = posicao_Caixa.y + tamanho_Caixa.y;
+			posicao_Entidade.y = posicao_Caixa.y + tamanho_Caixa.y;
 		}
 	}
 
-	pJ->setPosicao(posicao_Jogador);
-}
+	pE->setPosicao(posicao_Entidade);
 
+}
 
 
 
