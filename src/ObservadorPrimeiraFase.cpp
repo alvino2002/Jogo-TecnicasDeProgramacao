292ed58e4368ddde9e6c0d70fe5a5c72b4886e08
alvino2002 <alvino_yu@hotmail.com>
2025-06-08 20:19:29 -0300

    Create ObservadorPrimeiraFase.cpp

---
+++ src/ObservadorPrimeiraFase.cpp
@@ -0,0 +1,45 @@
+#include "ObservadorPrimeiraFase.h"
+#include "EstadoPause.h"
+#include <iostream>
+#include "EstadoSegundaFase.h"
+#include "GerenciadorEstado.h"
+#include "EstadoRanking.h"
+#include "EstadoFim.h"
+
+
+using namespace Masmorra::Observadores;
+
+
+ObservadorPrimeiraFase::ObservadorPrimeiraFase() :
+	Observador()
+{
+
+}
+
+ObservadorPrimeiraFase::~ObservadorPrimeiraFase()
+{
+
+}
+
+void ObservadorPrimeiraFase::notificarEvento(const std::string& evento)
+{
+	if (evento == "PAUSE")
+	{
+		Estados::EstadoPause* pause = new Estados::EstadoPause();
+		pGEs->adicionarEstado(pause);
+	}
+
+	if (evento == "FIM")
+	{
+		Estados::EstadoFim* fim = new Estados::EstadoFim();
+		pGEs->adicionarEstado(fim);
+	}
+
+	if (evento == "AVANCAR")
+	{
+		pGEs->removerEstado(); // Removendo o estado atual no topo, no caso EstadoPrimeiraFase
+		Estados::EstadoSegundaFase* segundaFase = new Estados::EstadoSegundaFase(1);
+		pGEs->adicionarEstado(segundaFase);
+	}
+}
+
