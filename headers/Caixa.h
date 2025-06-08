4293475e52c5a6c400c459ae969ab2e418e0fe1d
alvino2002 <alvino_yu@hotmail.com>
2025-06-08 12:15:02 -0300

    namespace adicionado

--- headers/Caixa.h
+++ headers/Caixa.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "Obstaculo.h"
 #include "Jogador.h"
-#include "GerenciadorTempo.h"
-#include "GerenciadorAnimacao.h"
 #include "GerenciadorColisao.h"
 
 namespace Masmorra
@@ -20,7 +18,7 @@ namespace Masmorra
 				Caixa(const sf::Vector2f tam, sf::Vector2f posIni, sf::Texture* texture, sf::Vector2u imageCount);
 				~Caixa();
 				void executar();
-				void interagir(Personagens::Jogador* pJ); // Obstacularizar;
+				void interagir(Entidade* pE); // Obstacularizar;
 			};
 		}
 	}
