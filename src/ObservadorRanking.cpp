21b671c30bf822cf682fe749bbd10ab73be78c9d
alvino2002 <alvino_yu@hotmail.com>
2025-06-08 20:20:05 -0300

    Create ObservadorRanking.cpp

---
+++ src/ObservadorRanking.cpp
@@ -0,0 +1,24 @@
+#include "ObservadorRanking.h"
+#include <iostream>
+#include "GerenciadorEstado.h"
+
+
+using namespace Masmorra::Observadores;
+
+ObservadorRanking::ObservadorRanking() :
+	Observador()
+{
+
+}
+
+ObservadorRanking::~ObservadorRanking()
+{
+
+}
+
+void ObservadorRanking::notificarEvento(const std::string& evento)
+{
+
+}
+
+
