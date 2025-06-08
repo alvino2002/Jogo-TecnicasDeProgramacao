5293fc44d7c268e963747a47ebeb1ddb7ca8884b
alvino2002 <alvino_yu@hotmail.com>
2025-06-08 19:46:20 -0300

    Update Fase.cpp

--- src/Fase.cpp
+++ src/Fase.cpp
@@ -1,12 +1,81 @@
 #include "Fase.h"
+#include <iostream>
+#include "GerenciadorColisao.h"
 
 
-Fase::Fase() :
-	Ente()
+using namespace Masmorra::Fases;
+
+
+Fase::Fase(int qtdeJogadores):
+	Ente(),
+	listaEntidades(),
+	camera(),
+	numeroInimigos(),
+	fimDaFase(false)
 {
+	if (qtdeJogadores == 1)
+	{
+		sf::Texture* texturaJog1 = new sf::Texture();
+		texturaJog1->loadFromFile("knight_Sprite.png");
+		pCav = new Entidades::Personagens::Cavaleiro(
+			sf::Vector2f(50.f, 70.f),
+			sf::Vector2f(100.f, 4800.f),
+			sf::Vector2f(200.f, 0.f),
+			100, texturaJog1, sf::Vector2u(4, 4));
+
+		pCur = nullptr;
+
+		pGC->incluirJogador(pCav);
+		Entidades::Obstaculos::Obstaculo::setCavaleiro(pCav); // Inimigos conhecerem o cavaleiro
+		Entidades::Personagens::Inimigo::setCavaleiro(pCav);
+	
+	}
+
+	else // qtdeJogadores == 2;
+	{
+		sf::Texture* texturaJog1 = new sf::Texture();
+		texturaJog1->loadFromFile("knight_Sprite.png");
+		pCav = new Entidades::Personagens::Cavaleiro(
+			sf::Vector2f(50.f, 70.f),
+			sf::Vector2f(100.f, 4800.f),
+			sf::Vector2f(200.f, 0.f),
+			100, texturaJog1, sf::Vector2u(4, 4));
+		sf::Texture* texturaJog2 = new sf::Texture();
+
+		texturaJog2->loadFromFile("healer_Sprite.png");
+		pCur = new Entidades::Personagens::Curandeira(
+			sf::Vector2f(50.f, 70.f),
+			sf::Vector2f(120.f, 4800.f),
+			sf::Vector2f(200.f, 0.f),
+			30, texturaJog2, sf::Vector2u(3, 4));
+
+		Entidades::Obstaculos::Obstaculo::setCurandeira(pCur); // Obstaculos conhecerem a curandeira
+		Entidades::Personagens::Inimigo::setCurandeira(pCur);
+
+		Entidades::Obstaculos::Obstaculo::setCavaleiro(pCav); // Inimigos conhecerem o cavaleiro
+		Entidades::Personagens::Inimigo::setCavaleiro(pCav);
+
+
+		pCur->setCavaleiro(pCav);
+
+		pGC->incluirJogador(pCav);
+		pGC->incluirJogador(pCur);
+	}
+
 }
 
 Fase::~Fase()
 {
 }
+
+void Fase::setFimDaFase(bool fim)
+{
+	fimDaFase = fim;
+}
+
+bool Fase::getFimDaFase()
+{
+	return fimDaFase;
+}
+
 
