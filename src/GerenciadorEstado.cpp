34838b22bfaaf191716d6bfdb83ce7ff27b83334
alvino2002 <alvino_yu@hotmail.com>
2025-06-08 19:49:35 -0300

    Create GerenciadorEstado.cpp

---
+++ src/GerenciadorEstado.cpp
@@ -0,0 +1,65 @@
+#include "GerenciadorEstado.h"
+
+
+using namespace Masmorra::Gerenciadores;
+
+
+GerenciadorEstado::GerenciadorEstado()
+{
+}
+
+GerenciadorEstado* GerenciadorEstado::getGerenciadorEstado()
+{
+	if (pGerenciadorEstado == nullptr)
+	{
+		pGerenciadorEstado = new GerenciadorEstado();
+	}
+	else
+	{
+		return pGerenciadorEstado;
+	}
+}
+
+
+
+GerenciadorEstado::~GerenciadorEstado()
+{
+}
+
+void GerenciadorEstado::adicionarEstado(Estados::Estado* estado)
+{
+	if (estado)
+	{
+		pilhaEstados.push(estado);
+
+	}
+}
+
+void GerenciadorEstado::removerEstado()
+{
+	if (pilhaEstados.top() != nullptr)
+	{
+		delete (pilhaEstados.top());
+		pilhaEstados.top() = nullptr;
+		pilhaEstados.pop();
+	}
+}
+
+void GerenciadorEstado::executar()
+{
+	if (!pilhaEstados.empty() && pilhaEstados.top() != nullptr)
+	{
+		pilhaEstados.top()->executar();
+		pilhaEstados.top()->verificarEvento();
+	}
+}
+
+/*Estado* GerenciadorEstado::getEstadoAtual() {
+	if (!pilhaEstados.empty()) {
+		return pilhaEstados.top();
+	}
+	return nullptr;
+}*/
+
+
+GerenciadorEstado* GerenciadorEstado::pGerenciadorEstado = nullptr;
