cb2f0bf1d5667ec4529606620bc2dbabcfab3554
alvino2002 <alvino_yu@hotmail.com>
2025-06-08 13:35:29 -0300

    Update Fase.h

--- headers/Fase.h
+++ headers/Fase.h
@@ -1,16 +1,32 @@
 #pragma once
 #include "Ente.h"
 #include "Entidade.h"
+#include "Cavaleiro.h"
+#include "Curandeira.h"
+#include "ListaEntidades.h"
 
-
-class Fase : public Masmorra::Ente
+namespace Masmorra
 {
-public:
-	Fase();
-	~Fase();
-	virtual void criarJogadores() = 0;
-	virtual void criarCenario() = 0;
-	virtual void criarInimigos() = 0;
-	virtual void executar() = 0;
-};
+	namespace Fases
+	{
+		class Fase : public Ente
+		{
+		protected:
+			Entidades::Personagens::Cavaleiro* pCav;
+			Entidades::Personagens::Curandeira* pCur;
+			Listas::ListaEntidades listaEntidades;
+			sf::View camera;
+			int numeroInimigos;
+			bool fimDaFase;
 
+		public:
+			Fase(int qtdeJogadores);
+			~Fase();
+			virtual void criarCenario() = 0;
+			virtual void criarInimigos() = 0;
+			virtual void executar() = 0;
+			void setFimDaFase(bool fim); // Fase acaba quando todos os inimigos forem derrotados
+			bool getFimDaFase();
+		};
+	}
+}
