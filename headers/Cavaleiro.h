c33b1d08a9d3e77623975cf0a8428f54b9e6e79c
alvino2002 <alvino_yu@hotmail.com>
2025-06-08 12:15:29 -0300

    namespace adicionado

--- headers/Cavaleiro.h
+++ headers/Cavaleiro.h
@@ -3,7 +3,6 @@
 #include "GerenciadorAnimacao.h"
 #include "GerenciadorTempo.h"
 
-
 namespace Masmorra
 {
 	namespace Entidades
@@ -22,18 +21,23 @@ namespace Masmorra
 				sf::Clock tempoAtaque;  // Temporizador para o ataque
 				float recargaAtaque;  // Tempo de espera entre ataques
 				bool ataquePronto;
+				static int inimigosEliminados; // Apenas cavaleiro consegue eliminar 
 				Gerenciadores::GerenciadorAnimacao* pGA;
 
 			public:
 				Cavaleiro(sf::Vector2f tamanho, sf::Vector2f posicaoInicial, sf::Vector2f velocidade, int vida,
-					      sf::Texture* texture, sf::Vector2u imageCount);
+					sf::Texture* texture, sf::Vector2u imageCount);
 				~Cavaleiro();
 				void executar();
 				bool getEstaAtacando()const;
 				void setEstaAtacando(bool atacando);
 				int getDano()const;
-				void operator--();
+				void operator--(); // Decrementar energia
 				void recuperar();
+				void operator++(); // Incrementar numero de inimigos eliminados
+				static int getInimigosEliminados();
+				static void setInimigosEliminados(int numeroInimigos);
+				static void zerarInimigosEliminados();
 			};
 		}
 	}
