a4071344875ae3b052ad140d3a155f945ba63196
alvino2002 <alvino_yu@hotmail.com>
2025-06-09 13:28:35 -0300

    Create EstadoRanking.h

---
+++ headers/EstadoRanking.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Ranking.h"
+#include "Estado.h"
+#include "ObservadorRanking.h"
+
+namespace Masmorra
+{
+	namespace Estados
+	{
+		class EstadoRanking : public Estado
+		{
+		private:
+			Ranking* pR;
+			Observadores::ObservadorRanking* pObs;
+
+		public:
+			EstadoRanking(int pontuacao);
+			~EstadoRanking();
+			void executar();
+			void verificarEvento();
+		};
+	}
+}
