09135174ca8de5865def02796a49e3382223d637
alvino2002 <alvino_yu@hotmail.com>
2025-06-13 21:43:23 -0300

    Update Esqueleto.h

--- headers/Esqueleto.h
+++ headers/Esqueleto.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "Inimigo.h"
 #include "GerenciadorAnimacao.h"
-#include "GerenciadorTempo.h"
+
 
 namespace Masmorra
 {
@@ -13,21 +13,22 @@ namespace Masmorra
 			{
 			private:
 				int batida;
-				Gerenciadores::GerenciadorAnimacao* pGA;
 
 			public:
 				Esqueleto(
+					int id,
 					sf::Vector2f tamanho,
-					sf::Vector2f posicaoInicial,
+					sf::Vector2f posicao,
 					sf::Vector2f velocidade,
 					int vida,
+					int nivelMaldade,
 					sf::Vector2f range,
 					sf::Texture* textura,
 					sf::Vector2u imageCount
 				);
 				~Esqueleto();
 				void executar();
-				void interagir(Jogador* pJ);
+				void danificar(Jogador* pJ);
 				void bater(Jogador* pJ);
 			};
 		}
