b0c452478a20972406822354c9886a81fa57d00c
alvino2002 <alvino_yu@hotmail.com>
2025-06-14 22:26:58 -0300

    Update Pause.h

--- headers/Pause.h
+++ headers/Pause.h
@@ -2,24 +2,24 @@
 #include "Ente.h"
 #include <iostream>
 #include "SFML/Graphics.hpp"
+#include "Interface.h"
 
 namespace Masmorra
 {
-	class Pause : public Ente
+	namespace Interfaces
 	{
-	private:
-		sf::Font* fonte;
-		sf::Texture* imagem;
-		sf::Sprite* fundo;
-		std::vector<const char*> opcoes;
-		std::vector<sf::Vector2f> coordenadas;
-		std::vector<sf::Text> textos;
-		std::vector<std::size_t> tamanhos;
+		class Pause : public Interface
+		{
+		private:
+			std::vector<std::string> opcoes;
+			std::vector<sf::Vector2f> coordenadas;
+			std::vector<std::size_t> tamanhos;
 
-	public:
-		Pause();
-		~Pause();
-		void executar();
-		void desenhar();
-	};
+		public:
+			Pause(int id);
+			~Pause();
+			void executar();
+			void inicializar();
+		};
+	}
 }
