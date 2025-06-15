e58f31f928d781e0a7c1edcfe5bb646f7c075c95
alvino2002 <alvino_yu@hotmail.com>
2025-06-15 19:48:07 -0300

    Update Observador.cpp

--- src/Observador.cpp
+++ src/Observador.cpp
@@ -1,16 +1,17 @@
 #include "Observador.h"
+#include "GerenciadorEstado.h"
 
 
 using namespace Masmorra::Observadores;
 
 
-Observador::Observador() :
-	Ente()
+Observador::Observador() 
 {
-
+	pGEs = Masmorra::Gerenciadores::GerenciadorEstado::getGerenciadorEstado();
 }
 
 Observador::~Observador()
 {
-
 }
+
+Masmorra::Gerenciadores::GerenciadorEstado * Observador::pGEs = nullptr;
