f9322b729cdc21ebb28dc07b5a97517617f9561d
alvino2002 <alvino_yu@hotmail.com>
2025-06-15 16:49:06 -0300

    Update EstadoPause.cpp

--- src/EstadoPause.cpp
+++ src/EstadoPause.cpp
@@ -1,15 +1,20 @@
 #include "EstadoPause.h"
 #include "GerenciadorGrafico.h"
+#include "GerenciadorColisao.h"
+#include "PrimeiraFase.h"
 
 
 using namespace Masmorra::Estados;
+
+using namespace Masmorra::Interfaces;
 
 
 EstadoPause::EstadoPause() :
 	Estado()
 {
-	pPause = new Pause();
+	pPause = new Pause(2);
 	pObs = new Observadores::ObservadorPause();
+	pObs->setPause(pPause);
 }
 
 EstadoPause::~EstadoPause()
@@ -21,11 +26,12 @@ EstadoPause::~EstadoPause()
 void EstadoPause::executar()
 {
 	pPause->executar();
+	pGT->reiniciar(); // Devemos ressetar o relogio para que nao haja acumulo de velocidade 
 }
 
 void EstadoPause::verificarEvento()
 {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
 	{
 		if (pObs)
 		{
@@ -33,12 +39,11 @@ void EstadoPause::verificarEvento()
 		}
 	}
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 	{
 		if (pObs)
 		{
 			pObs->notificarEvento("SAIR");
 		}
 	}
-
 }
